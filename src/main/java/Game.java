64626ac49cb3de4db02aaf9b142e6ce56d16403d
Alexandr <a-koshkarov@mail.ru>
2024-11-03 14:16:41 +0500

    Modified code Game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,39 +1,57 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class Game {
 
-    private int wins = 0;
-    private int losses = 0;
+    private int simulations;
 
-    public void playGame(boolean switchDoor) {
-        Random random = new Random();
-        int winDoor = random.nextInt(3); // выигрышная дверь
-        int playerChoice = random.nextInt(3); // Выбор участника
-
-        int openDoorHost;
-        do {
-            openDoorHost = random.nextInt(3);
-        } while (openDoorHost == winDoor || openDoorHost == playerChoice);
-
-        // Если игрок решает поменять выбор
-        if (switchDoor) {
-            playerChoice = 3 - playerChoice - openDoorHost; // Меняем выбор
-        }
-
-        // Проверяем, выиграл ли игрок
-        if (playerChoice == winDoor) {
-            wins++;
-        } else {
-            losses++;
-        }
-
+    public Game(int simulations) {
+        this.simulations = simulations;
     }
 
-    public HashMap<String, Integer> getResults() {
-        HashMap<String, Integer> results = new HashMap<>();
-        results.put("Wins", wins);
-        results.put("Losses", losses);
+    public Map<Integer, Boolean> playGame(boolean switchDoor) {
+        Random random = new Random();
+        Map<Integer, Boolean> results = new HashMap<>();
+
+        for (int i = 0; i < simulations; i++) {
+
+            int winDoor = random.nextInt(3); // выигрышная дверь
+            int playerChoice = random.nextInt(3); // Выбор участника
+
+            int openDoorHost;
+            do {
+                openDoorHost = random.nextInt(3);
+            } while (openDoorHost == winDoor || openDoorHost == playerChoice);
+
+            // Если игрок решает поменять выбор
+            if (switchDoor) {
+                playerChoice = 3 - playerChoice - openDoorHost; // Меняем выбор
+            }
+
+            // Проверяем, выиграл ли игрок
+            boolean res = (playerChoice == winDoor);
+            results.put(i, res);
+        }
+
         return results;
+    }
+
+    public void showResults(Map<Integer, Boolean> results) {
+        int wins = 0;
+        int losses = 0;
+
+        for (boolean result : results.values()) {
+            if (result) {
+                wins++;
+            } else {
+                losses++;
+            }
+        }
+
+        System.out.println("Статистика после " + simulations+ " игр:");
+        System.out.println("Победы: " + wins);
+        System.out.println("Поражения: " + losses);
+
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,18 @@
 import javax.swing.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Game game = new Game();
-        Game game1 = new Game();
-        int simulations = 1000;
+        Game game = new Game(1000);
+        Game game1 = new Game(500);
 
-        for (int i = 0; i < simulations; i++) {
-            game.playGame(true); // Игрок всегда меняет выбор
-        }
+        Map<Integer, Boolean> resultsGame = game.playGame(true);
+        Map<Integer, Boolean> resultsGame1 = game1.playGame(false);
 
-        for (int i = 0; i < simulations; i++) {
-            game1.playGame(false); // Игрок всегда оставляет свой выбор
-        }
-
-        HashMap<String, Integer> results = game.getResults();
-        System.out.println("Статистика после " + simulations + " игр:");
-        System.out.println("Победы: " + results.get("Wins"));
-        System.out.println("Поражения: " + results.get("Losses"));
-        System.out.println("Следующая игра");
-        HashMap<String, Integer> results1 = game1.getResults();
-        System.out.println("Статистика после " + simulations + " игр:");
-        System.out.println("Победы: " + results1.get("Wins"));
-        System.out.println("Поражения: " + results1.get("Losses"));
-
+        game.showResults(resultsGame);
+        game1.showResults(resultsGame1);
     }
 }
